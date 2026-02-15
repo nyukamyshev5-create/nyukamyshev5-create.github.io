117e3d5c6ac77dec6a275dc8bed17a5a6b710187
nyukamyshev5-create <nyukamyshev.5@gmail.com>
2026-02-15 17:43:41 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+const CONFIG = {
+  owner: "nyukamyshev5-create",
+  repo: "nyukamyshev5-create.github.io",
+  file: "data.json",
+  branch: "main",
+
+  token: "PASTE_YOUR_TOKEN_HERE",
+
+  adminPassword: "1234"
+};
