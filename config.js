12038c355c06d2f1050a59104f16dd3ad0885578
nyukamyshev5-create <nyukamyshev.5@gmail.com>
2026-02-15 19:56:17 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,7 @@
 const CONFIG = {
-  owner: "nyukamyshev5-create",
-  repo: "nyukamyshev5-create.github.io",
-  file: "data.json",
-  branch: "main",
-
-  token: "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",
-
-  adminPassword: "tutorai"
-};
+owner: "nyukamyshev5-create",
+repo: "nyukamyshev5-create.github.io",
+file: "data.json",
+branch: "main",
+token: "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"
+}
